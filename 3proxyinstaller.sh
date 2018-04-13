e9d95a92e1f8bd37af3fb5723b1a8652bd465423
Helder <trentjnr@aol.com>
2018-04-13 12:34:15 +0100

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,18 +1,18 @@
-apt-get update && apt-get -y upgrade
-apt-get install -y build-essential nano
-wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.11.tar.gz
+sudo apt-get update && apt-get -y upgrade
+sudo apt-get install -y build-essential nano
+sudo wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.11.tar.gz
 tar xzf 0.8.11.tar.gz
 cd 3proxy-0.8.11
-make -f Makefile.Linux
+sudo make -f Makefile.Linux
 cd src
-mkdir /etc/3proxy/
-mv 3proxy /etc/3proxy/
+sudo mkdir /etc/3proxy/
+sudo mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/3proxy.cfg
-chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/.proxyauth
-chmod 600 /etc/3proxy/.proxyauth
+sudo wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/3proxy.cfg
+sudo chmod 600 /etc/3proxy/3proxy.cfg
+sudo wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/.proxyauth
+sudo chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/3proxyinit
-chmod  +x /etc/init.d/3proxyinit
-update-rc.d 3proxyinit defaults
+sudo wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/3proxyinit
+sudo chmod  +x /etc/init.d/3proxyinit
+sudo update-rc.d 3proxyinit defaults

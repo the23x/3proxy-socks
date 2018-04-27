547467a5d41cd375b10c380f0cab392d74f398b7
the23x <2.b@bk.ru>
2018-04-27 15:11:25 +0300

    Update 3proxy_installerX.sh

--- 3proxy_installerX.sh
+++ 3proxy_installerX.sh
@@ -9,12 +9,12 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/3proxyX.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/the23x/3proxy-socks/master/3proxyX.cfg
 chmod 600 /etc/3proxy/3proxyX.cfg
 mv 3proxyX.cfg 3proxy.cfg
-wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/.proxyauth
+wget --no-check-certificate https://raw.githubusercontent.com/the23x/3proxy-socks/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/3proxyinit
+wget --no-check-certificate https://raw.githubusercontent.com/the23x/3proxy-socks/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
